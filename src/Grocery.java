78f84c8198cf428ab0c9c2d2f65114c263591b2f
Yuvraj Goyal <68333785+mrManiac-uv@users.noreply.github.com>
2020-07-21 02:54:40 +0530

    Update Grocery.java

--- src/Grocery.java
+++ src/Grocery.java
@@ -9,7 +9,7 @@ public class Grocery
         Scanner in=new Scanner(System.in);
         ArrayList<String> List= new ArrayList<>();
         String item, s, t;
-        int a, i, n;
+        int r, a, i, n;
         boolean flag = true;
 //        For text file creation
         FileWriter write=new FileWriter("C:\\Users\\richy\\OneDrive\\Desktop\\Grocery List.txt");
@@ -17,21 +17,36 @@ public class Grocery
         PrintWriter prnt=new PrintWriter(bf);
 //        Intro
         System.out.println("Grocer List-\n");
+//        Options
         System.out.println("Choose an option below:\n" +
-                "1. add item\t\t\t4. show the list\n2. remove item\t\t5. download list\n3. modify item\t\t0. exit");
+                "1. show options\t\t\t5. modify item\n2. add item\t\t\t\t6. print grocery list\n3. add many items\t\t7. download grocery list\n4. remove item\t\t\t0. exit");
 //        Choose any option
         while(true)
         {
-            System.out.print("\nNext: ");
+            System.out.print("\nChoice: ");
             a = in.nextInt();
+//            Show Options
+            if (a==1)
+                System.out.println("Choose an option below:\n" +
+                        "1. show options\t\t\t5. modify item\n2. add item\t\t\t\t6. print grocery list\n3. add many items\t\t7. download grocery list\n4. remove item\t\t\t0. exit");
 //            Add item
-            if (a==1) {
+            if (a==2) {
                 System.out.print("Add: ");
                 List.add(in.next());
                 System.out.println("Item enlisted.");
             }
+//            Add item(s)
+            if (a==3) {
+                System.out.println("How many items are to be added?");
+                r = in.nextInt();
+                for (i=0; i<r; i++) {
+                    System.out.print("Add: ");
+                    List.add(in.next());
+                }
+                System.out.println("All items enlisted.");
+            }
 //            Remove item
-            if (a==2) {
+            if (a==4) {
                 System.out.print("Item to be removed: ");
                 item = in.next();
 //                To check if input is item name or item number
@@ -49,7 +64,7 @@ public class Grocery
                 System.out.println("Item deleted.");
             }
 //            Modify item
-            if (a==3) {
+            if (a==5) {
                 System.out.print("Item to be edited: ");
                 item = in.next();
                 n = -2;
@@ -72,19 +87,18 @@ public class Grocery
                     List.add(n, in.next());
                     flag = false;
                 }
-                System.out.println(n);
                 if (!flag)
                     List.remove(n+1);
                 System.out.println("Item modified.");
             }
 //            Print list
-            if (a==4){
+            if (a==6){
                 System.out.println("Your Grocery List: ");
                 for (i=0; i<List.size(); i++)
                 System.out.println((i+1)+": "+List.get(i));
             }
 //            Download the List
-            if (a==5){
+            if (a==7){
                 prnt.println("Your Grocery List: ");
                 for (i=0; i<List.size(); i++)
                     prnt.println((i+1)+": "+List.get(i));
